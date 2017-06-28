08dea22efe930d65f50de8a66c57e4b56827d2bb
Christoph Wirsching <Christoph@Christophs-MBP.fritz.box>
2017-06-28 20:48:22 +0200

    Kopier Schnipsel

---
+++ js/Kopier Schnipsel.js
@@ -0,0 +1,12 @@
+/*Kleine Snippets zum kopieren*/
+$( document ).ready(function(){
+
+});
+
+$( "CLICK_CONTAINER" ).click(function(){
+
+}); 
+
+$( "CLICK_CONTAINER" ).on('click', function (e) {
+    e.preventDefault();
+});
